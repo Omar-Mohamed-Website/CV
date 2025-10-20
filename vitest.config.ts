fda9eb0293b6c93bd31261606dc3e8ce101ceba7
Mete Yalcinkaya <ymete089@gmail.com>
2025-10-20 13:58:43 +0300

    fix: remove type assertion from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()] as any,
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
